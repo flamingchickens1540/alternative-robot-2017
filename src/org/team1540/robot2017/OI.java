7bcce90a2a8855f073945833356ba565e1163f5a
wangl5 <github@liam.eml.cc>
2017-02-11 12:20:48 -0800

    Moved controls assignments to IO

--- src/org/team1540/robot2017/OI.java
+++ src/org/team1540/robot2017/OI.java
@@ -7,12 +7,29 @@ import edu.wpi.first.wpilibj.Joystick;
  * interface to the commands and command groups that allow control of the robot.
  */
 public class OI {
-	public static final Joystick primary = new Joystick(0);
-	public static final Joystick secondary = new Joystick(1);
+	// Joysticks
+	private static final Joystick primary = new Joystick(0);
+	private static final Joystick secondary = new Joystick(1);
 	
-	public static final int primaryRightAxis = 5;
-	public static final int primaryLeftAxis = 1;
+	private static final int rightAxisX = 5;
+	private static final int rightAxisY = 5;
+	private static final int leftAxisX = 1;
+	private static final int leftAxisY = 1;
 	
-	public static final int primaryRightTrigger = 3;
-	public static final int primaryLeftTrigger = 2;
+	private static final int rightTrigger = 3;
+	private static final int leftTrigger = 2;
+	
+	// Climber
+	public static double getClimberJoystick() {
+		return secondary.getRawAxis(leftAxisY);
+	}
+	
+	// DriveTrain
+	public static double getDriveRightJoystick() {
+		return primary.getRawAxis(rightAxisY);
+	}
+	public static double getDriveLeftJoystick() {
+		return primary.getRawAxis(leftAxisY);
+	}
+	
 }
