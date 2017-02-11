ecdcf74df1fb2ad517c525d8281fbedf400b7827
wangl5 <github@liam.eml.cc>
2017-02-11 12:06:11 -0800

    Corrected CAN ids

--- src/org/team1540/robot2017/RobotMap.java
+++ src/org/team1540/robot2017/RobotMap.java
@@ -7,29 +7,34 @@ package org.team1540.robot2017;
  * floating around.
  */
 public class RobotMap {
-	public static final int climberTalonTop = 0;
-	public static final int climberTalonBottom = 2;
-	public static final int feederTalonTop = 10;
-	public static final int feederTalonLeft = 4;
-	public static final int feederTalonRight = 6;
-	public static final int gearSliderTalon = 11;
+	
+	// Climber
+	public static final int climberTop = 5;
+	public static final int climberBottom = 2;
+	
+	// Feeder
+	public static final int feederBelt = 12;
+	public static final int feederConveyor = 3;
+	public static final int feederFunnelingRollerRight = 1;
+	public static final int feederFunnelingRollerLeft = 7;
+	
+	// GearMechanism
+	public static final int gearSliderTalon = 8;
 //	public static final int gearServoRightDeploy;
 //	public static final int gearServoLeftDeploy;
-	public static final int intakeTalon = 7;
-	public static final int shooterTalonRightFlywheel = 10;
-	public static final int shooterTalonLeftFlywheel = 11;
-	public static final int shooterTalonBelt = 8;
-//	public static final int driveTalonRightA = 12;
-//	public static final int driveTalonRightB = 13;
-//	public static final int driveTalonRightC = 1;
-//	public static final int driveTalonLeftA = 3;
-//	public static final int driveTalonLeftB = 15;
-//	public static final int driveTalonLeftC = 14;
 	
-	public static final int driveTalonRightA = 1;
-	public static final int driveTalonRightB = 2;
-	public static final int driveTalonRightC = 3;
-	public static final int driveTalonLeftA = 4;
-	public static final int driveTalonLeftB = 5;
-	public static final int driveTalonLeftC = 6;
+	// Intake
+	public static final int intakeRollers = 6;
+	
+	// Shooter
+	public static final int shooterRightFlywheel = 10;
+	public static final int shooterLeftFlywheel = 9;
+	
+	// DriveTrain
+	public static final int driveTalonRightA = 13;
+	public static final int driveTalonRightB = 14;
+	public static final int driveTalonRightC = 4;
+	public static final int driveTalonLeftA = 11;
+	public static final int driveTalonLeftB = 15;
+	public static final int driveTalonLeftC = 16;
 }
