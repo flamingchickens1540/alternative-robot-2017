ee640b65a60221cc2b52833aaf3f27b9d98432e6
wangl5 <github@liam.eml.cc>
2017-02-12 12:31:48 -0800

    Added all xbox controller buttons to OI

--- src/org/team1540/robot2017/OI.java
+++ src/org/team1540/robot2017/OI.java
@@ -11,13 +11,33 @@ public class OI {
 	public static final Joystick primary = new Joystick(0);
 	public static final Joystick secondary = new Joystick(1);
 	
-	private static final int rightAxisX = 5;
+	private static final int rightAxisX = (Integer) null; // ??
 	private static final int rightAxisY = 5;
-	private static final int leftAxisX = 1;
+	private static final int leftAxisX = (Integer) null; // ??
 	private static final int leftAxisY = 1;
 	
 	private static final int rightTrigger = 3;
 	private static final int leftTrigger = 2;
+	
+	private static final int rightBumper = (Integer) null;
+	private static final int leftBumper = (Integer) null;
+	
+	private static final int buttonX = (Integer) null;
+	private static final int buttonA = (Integer) null;
+	private static final int buttonY = (Integer) null;
+	private static final int buttonB = (Integer) null;
+	
+	private static final int leftJoyPush = (Integer) null;
+	private static final int rightJoyPush = (Integer) null;
+	
+	private static final int buttonN = (Integer) null;
+	private static final int buttonNE = (Integer) null;
+	private static final int buttonE = (Integer) null;
+	private static final int buttonSE = (Integer) null;
+	private static final int buttonS = (Integer) null;
+	private static final int buttonSW = (Integer) null;
+	private static final int buttonW = (Integer) null;
+	private static final int buttonNW = (Integer) null;
 	
 	// Climber
 	public static double getClimberJoystick() {
@@ -31,5 +51,9 @@ public class OI {
 	public static double getDriveLeftJoystick() {
 		return primary.getRawAxis(leftAxisY);
 	}
-	
+
+	// GearMechanism
+	public static double getGearSliderJoystick() {
+		return secondary.getRawAxis(rightAxisY);
+	}
 }
