7d95bcbb6871d35d613f2c9e32c812a71d53ba13
Amber Merrill <merrillam@catlin.edu>
2017-02-17 10:59:13 -0800

    Changed shooter talon something

--- src/org/team1540/robot2017/subsystems/Shooter.java
+++ src/org/team1540/robot2017/subsystems/Shooter.java
@@ -11,8 +11,8 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 
 public class Shooter extends Subsystem {
 
-	private final CANTalon shooterFlywheelTalon = new CANTalon(RobotMap.shooterTalonLeftFlywheel);
-	private final CANTalon shooterRightFlywheelTalon = new CANTalon(RobotMap.shooterTalonRightFlywheel);
+	private final CANTalon shooterFlywheelTalon = new CANTalon(RobotMap.shooterTalonLeftFlywheel, 1);
+	private final CANTalon shooterRightFlywheelTalon = new CANTalon(RobotMap.shooterTalonRightFlywheel, 1);
 	
 	public Shooter() {
 		shooterRightFlywheelTalon.reset();
