d96ec1d5a366df20ddecd7c03e03623bb90c222c
Jake <me@jakespringer.com>
2017-02-21 15:18:24 -0800

    Reformatted KangarooVision

--- KangarooVision/src/org/team1540/vision/ui/VisionControl.java
+++ KangarooVision/src/org/team1540/vision/ui/VisionControl.java
@@ -2,56 +2,45 @@ package org.team1540.vision.ui;
 
 import java.awt.EventQueue;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
 import javax.swing.BoxLayout;
-import javax.swing.JTextPane;
-import java.awt.FlowLayout;
-import java.awt.Canvas;
-import javax.swing.JSplitPane;
-import java.awt.Color;
-import java.awt.Font;
-import javax.swing.JTextField;
-import javax.swing.JLabel;
-import java.awt.Component;
-import javax.swing.Box;
-import java.awt.Dimension;
+import javax.swing.JFrame;
 
 public class VisionControl {
 
-	private JFrame frame;
+    private JFrame frame;
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					VisionControl window = new VisionControl();
-					window.frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+    /**
+     * Launch the application.
+     */
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    VisionControl window = new VisionControl();
+                    window.frame.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
 
-	/**
-	 * Create the application.
-	 */
-	public VisionControl() {
-		initialize();
-	}
+    /**
+     * Create the application.
+     */
+    public VisionControl() {
+        initialize();
+    }
 
-	/**
-	 * Initialize the contents of the frame.
-	 */
-	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(0, 0, 480, 640);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(new BoxLayout(frame.getContentPane(), BoxLayout.Y_AXIS));
-	}
+    /**
+     * Initialize the contents of the frame.
+     */
+    private void initialize() {
+        frame = new JFrame();
+        frame.setBounds(0, 0, 480, 640);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().setLayout(new BoxLayout(frame.getContentPane(), BoxLayout.Y_AXIS));
+    }
 
 }
--- KangarooVision/src/org/team1540/vision/ui/WebcamPanel.java
+++ KangarooVision/src/org/team1540/vision/ui/WebcamPanel.java
@@ -7,34 +7,34 @@ import java.awt.image.BufferedImage;
 import javax.swing.JPanel;
 
 public class WebcamPanel extends JPanel {
-	private BufferedImage currentImage;
-	
-	public WebcamPanel(BufferedImage originalImage) {
-		super();
-		setImage(originalImage);
-	}
-	
-	public WebcamPanel() {
-		super();
-		setImage(null);
-	}
-	
-	public void setImage(BufferedImage newImage) {
-		currentImage = newImage;
-//		if (currentImage != null) {
-//			setBounds(0, 0, currentImage.getWidth(), currentImage.getHeight());
-//		} else {
-//			setBounds(0, 0, 0, 0);
-//		}
-		revalidate();
-		repaint();
-	}
-	
-	@Override
-	public void paint(Graphics graphics) {
-		Graphics2D g2 = (Graphics2D) graphics;
-		if (currentImage != null) {
-			g2.drawImage(currentImage, 0, 0, currentImage.getWidth(), currentImage.getHeight(), null);
-		}
-	}
+    private BufferedImage currentImage;
+
+    public WebcamPanel(BufferedImage originalImage) {
+        super();
+        setImage(originalImage);
+    }
+
+    public WebcamPanel() {
+        super();
+        setImage(null);
+    }
+
+    public void setImage(BufferedImage newImage) {
+        currentImage = newImage;
+        // if (currentImage != null) {
+        // setBounds(0, 0, currentImage.getWidth(), currentImage.getHeight());
+        // } else {
+        // setBounds(0, 0, 0, 0);
+        // }
+        revalidate();
+        repaint();
+    }
+
+    @Override
+    public void paint(Graphics graphics) {
+        Graphics2D g2 = (Graphics2D) graphics;
+        if (currentImage != null) {
+            g2.drawImage(currentImage, 0, 0, currentImage.getWidth(), currentImage.getHeight(), null);
+        }
+    }
 }
