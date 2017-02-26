669ec10a5b0982e51ae3c06faff34521053a7f85
EdelmanJM <edelmanjm@users.noreply.github.com>
2017-02-25 17:36:40 -0800

    Minor changes
    Added URL safety, although it's a little clunky.
    Redirected output to GUI.
    Added color picker, though it doesn't really do much right now.

--- DriveStation/src/org/team1540/client/ui/WebcamUI.java
+++ DriveStation/src/org/team1540/client/ui/WebcamUI.java
@@ -1,15 +1,22 @@
 package org.team1540.client.ui;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
 import java.net.MalformedURLException;
 
-import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JColorChooser;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 import com.github.sarxos.webcam.Webcam;
@@ -24,17 +31,33 @@ public class WebcamUI {
 	//http://89.203.137.209/axis-cgi/mjpg/video.cgi
 	//http://webcam1.lpl.org/axis-cgi/mjpg/video.cgi
 	public Dimension windowSize;
+	private Color boxColor; 
 	
-	private JFrame mainWindow = new JFrame("Webcam Stream");
-	private JTextField urlBar;
-	private WebcamPanel webcamView;
+	//indents show hierarchy, bottom to top
+		private JTextField urlBar;
+		private WebcamPanel webcamView;
+			private JButton pickColor = new JButton("Pick color");
+		private JPanel rightSide = new JPanel(new BorderLayout());
+			private JTextArea log = new JTextArea(10,100);
+		private JScrollPane logWindow = new JScrollPane(log);
+	private JFrame mainWindow = new JFrame("Webcam Stream");	
 	
+	/**
+	 * Initializes a WebcamUI with the default URL and the window size set to the 
+	 * screen size.
+	 */
 	public WebcamUI() {
 		windowSize = Toolkit.getDefaultToolkit().getScreenSize();
 		windowSize.setSize(windowSize.getWidth(), windowSize.getHeight());
+		
 		initialize();
 	}
 	
+	/**
+	 * Initializes a WebcamUI with the given URL and the given window size.
+	 * @param url URL to use with the webcam
+	 * @param windowSize Window size to use.
+	 */
 	public WebcamUI(String url, Dimension windowSize) {
 		URL = url;
 		this.windowSize = windowSize;
@@ -43,57 +66,72 @@ public class WebcamUI {
 	}
 	
 	private void initialize() {
+		log.setEditable(false);
+		PrintStream printOut = new PrintStream(new LogOutput());
+		System.setOut(printOut);
+		System.setErr(printOut);
+		
 		mainWindow.setResizable(true);
 		mainWindow.setLayout(new BorderLayout());
 		mainWindow.setSize((int) windowSize.getWidth(), (int) windowSize.getHeight());
 		
 		urlBar = new JTextField(URL);
-		urlBar.addActionListener(new URLUpdate());
+		urlBar.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				update();
+			}
+		});
 		mainWindow.add(urlBar, BorderLayout.NORTH);
 		
 		Webcam.setDriver(new IpCamDriver());
 		update();
 		
+		pickColor.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				boxColor = JColorChooser.showDialog(null, "Choose a color", boxColor);	
+			}
+		});
+		rightSide.add(pickColor, BorderLayout.NORTH);
+		
+		mainWindow.add(rightSide, BorderLayout.EAST);
+		mainWindow.add(logWindow, BorderLayout.SOUTH);
+		
 		mainWindow.setVisible(true);
 	}
 	
-	private class URLUpdate implements ActionListener {
-		
-		@Override
-		public void actionPerformed(ActionEvent e) {
-			update();
-		}
-		
-	}
-	
 	private void update() {
-		JTextField textBox = new JTextField(System.currentTimeMillis() + "");
-		
 		if (URL.equals(urlBar.getText()) && webcamView != null) {
 			return;
 		}
 		URL = urlBar.getText();
-		if (webcamView != null) {
-			mainWindow.remove(webcamView);
-			//mainWindow.remove(textBox);
-			System.out.println("Removed");
-		}
 		try {
-			IpCamDeviceRegistry.unregisterAll();
 			IpCamDeviceRegistry.register(URL, URL, IpCamMode.PUSH);
 		} catch (MalformedURLException e) {
 			e.printStackTrace();
+			return;
 		}
-		System.out.println(Webcam.getWebcams());
+		if (Webcam.getWebcams().size() > 1) {
+			IpCamDeviceRegistry.unregister(Webcam.getWebcams().get(0).getName());	
+		}
+		if (webcamView != null) {
+			mainWindow.remove(webcamView);
+		}
 		webcamView = new WebcamPanel(Webcam.getWebcams().get(0));
 		webcamView.start();
-		//mainWindow.add(new JTextField(System.currentTimeMillis() + ""));
-		//mainWindow.add(textBox);
 		mainWindow.add(webcamView, BorderLayout.CENTER);
 		mainWindow.setTitle(URL);
 		mainWindow.revalidate();
 		mainWindow.repaint();
-		System.out.println("finished");
 	}
 	
+	private class LogOutput extends OutputStream {
+		
+		public void write(int b) throws IOException {
+			log.append(String.valueOf((char)b));
+			log.setCaretPosition(log.getDocument().getLength());
+		}
+		
+	}
 }
