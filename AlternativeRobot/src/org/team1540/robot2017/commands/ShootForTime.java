1bb10174f071f777092be46452ce5660ed83ef8c
Amber Merrill <merrillam@catlin.edu>
2017-02-28 17:04:36 -0800

    idk

---
+++ AlternativeRobot/src/org/team1540/robot2017/commands/ShootForTime.java
@@ -0,0 +1,16 @@
+package org.team1540.robot2017.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class ShootForTime extends Command {
+    
+    public ShootForTime() {
+        
+    }
+    
+    @Override
+    protected boolean isFinished() {
+        return false;
+    }
+    
+}
